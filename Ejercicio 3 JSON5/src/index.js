39917aca9539fd14f79f3fdbc0543c06b7eb0c00
AlvaroPulidoBernal <61841060+AlvaroPulidoBernal@users.noreply.github.com>
2023-10-08 21:18:46 -0600

    Se agregan archivos ejercicio 3

---
+++ Ejercicio 3 JSON5/src/index.js
@@ -0,0 +1,28 @@
+import _ from 'lodash';
+import './estilo.css';
+import Imagen from './imagen.png'
+import Datos from './datos.csv'
+import yaml from './datos.yaml'
+import json5 from './datos.json5'
+import './estilo.scss'
+
+function componente(){
+    const elemento = document.createElement('div');
+    //lodash simplifica operaciones sobre strings
+    elemento.innerHTML = _.join(['Hola','Webpack'],' ');
+    //fondo es la clase para el estilo que viene del estilo.scss
+    elemento.classList.add('fondo');
+
+    const miImagen = new Image();
+    miImagen.src = Imagen;
+    elemento.appendChild(miImagen);
+
+    console.log(Datos);
+
+    return elemento;
+}
+
+console.log(yaml.name);
+console.log(json5.name);
+
+document.body.appendChild(componente());
